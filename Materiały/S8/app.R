5e8218ea8f21ad5c5057b88d02e66ce866acf4fc
Piotr Podbielski <kontakt@piotrpodbielski.pl>
2019-04-15 13:17:29 +0200

    Modyfikacja kodu prowadzacego

--- Materiały/S8/app.R
+++ Materiały/S8/app.R
@@ -7,19 +7,25 @@ ui <- fluidPage(
   titlePanel("Simple Shiny App"),
   
   h2("Scatterplot"),
-  plotOutput("countries_plot", height = 600, click = "country_click")
+  plotOutput("countries_plot", height = 600, click = "country_click"),
+  verbatimTextOutput("country_click_value")
 )
 
 server <- function(input, output) {
+  
+  countries_reactive <- reactiveValues(countries)
+  output[["country_click_value"]] <- renderPrint({
+    str(nearPoints(countries, input[["country_click"]], maxpoints=1))
+  })
   
   continent_colors <- c(Asia = "red", Europe = "green", Africa = "orange", Americas = "black", 
                         Oceania = "blue")
   
   output[["countries_plot"]] <- renderPlot({
     ggplot(countries, aes(x = birth.rate, y = death.rate, color = continent)) +
-      geom_point() +
-      scale_color_manual(values = continent_colors) +
-      theme_bw()
+    geom_point() +
+    scale_color_manual(values = continent_colors) +
+    theme_bw()
   })
   
 }
